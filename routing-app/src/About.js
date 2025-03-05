9d325adaffd8259a8b574f8fc70c4a780ca14954
harshi75329 <harshithaadusumalli7@gmail.com>
2025-03-05 17:56:12 +0530

    react

---
+++ routing-app/src/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./studies.css";
+
+const About = () => {
+  return (
+    <div className="page">
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ routing-app/src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./studies.css";
+
+const Home = () => {
+  return (
+    <div className="page">
+      <h1>Home Page</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+};
+
+export default Home;
