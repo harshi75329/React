0e6e47fa583f3c4195f2c4388e9e5c3454544e66
harshi75329 <harshithaadusumalli7@gmail.com>
2025-03-06 18:11:26 +0530

    react appstore

--- e-commerce-store/src/App.js
+++ e-commerce-store/src/App.js
@@ -1,15 +1,28 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import LoginPage from "./Login";
 import ProductCatalogue from "./ProductCatalogue";
+import Cart from "./Cart";
+import Payment from "./Payment";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  const removeFromCart = (index) => {
+    setCart(cart.filter((_, i) => i !== index));
+  };
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/products" element={<ProductCatalogue />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/products" element={<ProductCatalogue addToCart={addToCart} />} />
+      <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart} />} />
+      <Route path="/payment" element={<Payment cart={cart} />} /> {/* ✅ Pass cart here */}
+    </Routes>
   );
 }
 
