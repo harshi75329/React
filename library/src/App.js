3c91be69a4fcad33487828493e661c1c675440c9
harshi75329 <harshithaadusumalli7@gmail.com>
2025-03-13 12:32:54 +0530

    LMS

---
+++ library/src/App.js
@@ -0,0 +1,47 @@
+// src/App.js
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import SignUpForm from "./SignUpForm";
+import Login from "./Login";
+import ProductCatalogue from "./ProductCatalogue";
+import Cart from "./Cart";
+import StartPage from "./StartPage";
+import AdminLogin from "./AdminLogin";
+import AdminPage from "./AdminPage";
+import AdminProductManagement from "./AdminProductManagement";
+import "./App.css";
+
+const App = () => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  const removeFromCart = (index) => {
+    setCart(cart.filter((_, i) => i !== index));
+  };
+
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> |  
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/products" element={<ProductCatalogue addToCart={addToCart} />} />
+          <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart} />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/admin-products" element={<AdminProductManagement />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
